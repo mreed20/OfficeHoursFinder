957b217fb315fe01173ff1b1655ce1a8ae900256
Michael Reed <michael@nobody.com>
2019-11-20 21:43:56 -0500

    Get MANYHOUR sort of (not reall) working

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,7 +1,10 @@
-class Paths
+class Constants
 {
     static final String MUSTACHE_SELECT_CLASS = "/select_class.mustache";
     static final String MUSTACHE_INDEX = "/index.mustache";
     static final String MUSTACHE_SELECT_AVAILABILITY = "/select_availability.mustache";
     static final String MUSTACHE_DISPLAY_GENERATED_HOURS = "/display_generated_hours.mustache";
+
+    static final String COOKIE_CURRENT_CLASS = "current_class";
+    static final String COOKIE_CURRENT_USER = "current_user";
 }
--- src/main/java/HoursFinder.java
+++ src/main/java/HoursFinder.java
@@ -1,6 +1,8 @@
+import com.github.mustachejava.util.DecoratedCollection;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import javax.annotation.processing.Generated;
 import java.sql.SQLException;
 import java.time.DayOfWeek;
 import java.time.Duration;
@@ -8,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAccessor;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 class HoursFinder
@@ -19,8 +22,17 @@ class HoursFinder
     private static final DateTimeFormatter dayOfWeekFormatter =
             DateTimeFormatter.ofPattern("E");
 
+    // Ugly hack to make list of time slots accessible across pages.
+    // Should probably use sessions instead.
+    private static final AtomicReference<List<TimeSlot>> selectedTimes =
+            new AtomicReference<>();
+    private static AtomicReference<List<GeneratedHour>> top10 =
+            new AtomicReference<>();
+
     public static void main(String[] args)
     {
+        // THIS IS A HACK
+        selectedTimes.set(new ArrayList<>());
 
         try {
             final String url = "jdbc:postgresql://localhost:5432/postgres";
@@ -39,7 +51,7 @@ class HoursFinder
 
         // Home page handler, which gets executed when we first visit the website
         // since we will be at the root directory ("/").
-        app.get("/", ctx -> ctx.render(Paths.MUSTACHE_INDEX));
+        app.get("/", ctx -> ctx.render(Constants.MUSTACHE_INDEX));
 
 
         // User login handler.
@@ -54,13 +66,14 @@ class HoursFinder
                         if (teacher != null) {
                             // Set a cookie on the client so that other pages know
                             // who is logged in, and thus what data to generate.
-                            ctx.cookieStore("current_user", gNumber);
+                            ctx.cookieStore(Constants.COOKIE_CURRENT_USER, gNumber);
+                            // Used for later after the user selects office hours.
                             renderSelectClass(ctx, teacher);
                         } else {
-                            ctx.render(Paths.MUSTACHE_INDEX, ErrorMaps.USER_NOT_FOUND);
+                            ctx.render(Constants.MUSTACHE_INDEX, ErrorMaps.USER_NOT_FOUND);
                         }
                     } catch (NumberFormatException e) {
-                        ctx.render(Paths.MUSTACHE_INDEX, ErrorMaps.INVALID_INPUT);
+                        ctx.render(Constants.MUSTACHE_INDEX, ErrorMaps.INVALID_INPUT);
                     }
                 }
         );
@@ -72,8 +85,8 @@ class HoursFinder
                     // have the `required` attribute, meaning that at least one radio button must be selected
                     // before the browser submits the form.
                     assert selection != null;
-                    ctx.cookieStore("current_class", selection);
-                    ctx.render(Paths.MUSTACHE_SELECT_AVAILABILITY);
+                    ctx.cookieStore(Constants.COOKIE_CURRENT_CLASS, selection);
+                    ctx.render(Constants.MUSTACHE_SELECT_AVAILABILITY);
                 }
         );
 
@@ -103,23 +116,50 @@ class HoursFinder
 
                     // Get the name of the currently selected class, which we stored
                     // in a cookie previously.
-                    String currentClassName = ctx.cookieStore("current_class");
+                    String currentClassName = ctx.cookieStore(Constants.COOKIE_CURRENT_CLASS);
                     assert currentClassName != null;
 
                     // Get the list of students in the class.
                     List<Student> students = new ArrayList<>(dc.getStudents(currentClassName));
                     // Generate the office hours.
                     ScheduleAnalyzer analyzer = new ScheduleAnalyzer(students, timeSlots);
+
+                    // Tell the ScheduleAnalyzer to account for previously selected office hours when scheduling.
+                    for (TimeSlot t : selectedTimes.get()) {
+                        analyzer.setOfficeHour(t);
+                    }
+
+                    // Finally generate the office hours.
                     List<GeneratedHour> hours = analyzer.buildGeneratedHours();
                     renderDisplayGeneratedHours(ctx, hours);
                 }
         );
 
         app.post("/generate_again", ctx -> {
-                    String selection = ctx.formParam("selection");
-                    assert selection != null;
-                    // Redirect to home page for now.
-                    ctx.html(selection);
+                    String nextPage = ctx.formParam("next_page");
+                    switch (nextPage) {
+                        case "same_class":
+                            // Store the selected time slot.
+                            int i = Integer.parseInt(ctx.formParam("timeslot_selection"));
+                            TimeSlot t = top10.get().get(i).getTimeSlot();
+                            selectedTimes.get().add(t);
+
+                            // Bring the user back to the availability selection screen
+                            ctx.render(Constants.MUSTACHE_SELECT_AVAILABILITY);
+                            break;
+                        case "new_class":
+                            // Clear, but do not delete, the selected hours list.
+                            // TODO: move this stuff inside main so we dont need to use an AtomicReference
+                            selectedTimes.get().clear();
+
+                            // Bring the user back to the class selection page.
+                            int gNumber = ctx.cookieStore(Constants.COOKIE_CURRENT_USER);
+                            Teacher teacher = dc.getTeacher(gNumber);
+                            renderSelectClass(ctx, teacher);
+                            break;
+                        default:
+                            throw new RuntimeException();
+                    }
                 }
         );
 
@@ -137,17 +177,19 @@ class HoursFinder
         Map<String, Object> model = new HashMap<>();
         // Get the current class from the cookie store.
         // We use it as a caption for the table of generated hours.
-        model.put("classname", ctx.cookieStore("current_class"));
+        model.put("classname", ctx.cookieStore(Constants.COOKIE_CURRENT_CLASS));
 
         // TODO: test that no hours coincide with the class being selected
         hours.sort(Comparator.comparing(GeneratedHour::getAvailPercent).reversed());
 
-        List<GeneratedHour> newList = hours.stream()
-                                           .limit(5)
-                                           .collect(Collectors.toList());
-        model.put("hours", newList);
+        // Get the top 5 time slots by availability percentage...
+        top10.set(hours.stream().limit(10).collect(Collectors.toList()));
+        // ... and wrap said time slots in a DecoratedCollection, which will
+        // expose {{index}} tags (via the iterator) to the underlying mustache
+        // template.
+        model.put("hours", new DecoratedCollection<>(top10.get()));
 
-        ctx.render(Paths.MUSTACHE_DISPLAY_GENERATED_HOURS, model);
+        ctx.render(Constants.MUSTACHE_DISPLAY_GENERATED_HOURS, model);
     }
 
 
@@ -159,6 +201,6 @@ class HoursFinder
         model.put("classes", teacher.classesTaught);
 
         // Render the mustache file with the given model.
-        ctx.render(Paths.MUSTACHE_SELECT_CLASS, model);
+        ctx.render(Constants.MUSTACHE_SELECT_CLASS, model);
     }
 }
